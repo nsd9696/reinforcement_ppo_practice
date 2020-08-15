59d4b60b3da15fdae2ee741502e4149d1f1b8827
nsd96 <nsd26534613@gmail.com>
2020-08-15 14:49:05 +0900

    print message commit

--- main.py
+++ main.py
@@ -108,7 +108,7 @@ def main():
     gamma = 0.99
     T = 20 # 몇 time step 동안 data를 모을지
     score = 0.0
-    print_intverval = 20
+    print_interval = 20
 
     for n_epi in range(10000):
         s = env.reset()
@@ -128,3 +128,12 @@ def main():
                 if done:
                     break
             model.train()
+
+        if n_epi%print_interval==0 and n_epi!=0:
+            print("# of episode :{}, avg score : {:.1f}".format(n_epi, score / print_interval))
+            score = 0.0
+
+    env.close()
+
+if __name__ == '__main__':
+    main()
